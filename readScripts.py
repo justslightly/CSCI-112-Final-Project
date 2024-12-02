4470145ac3286441036b6ba85c7e7294eaa7d7ed
just_slightly <63225387+justslightly@users.noreply.github.com>
2024-12-02 22:02:28 +0800

    transfer delete scripts

--- readScripts.py
+++ readScripts.py
@@ -343,19 +343,3 @@ def aggregateTotalBalanceByCurrency():
     ]
 
     results = collection.aggregate(pipeline)
-
-
-# DELETE (FOR REVIEW)
-
-# Deleting shares documents without any shares owned
-def deleteEmptyShares():
-    conn = openConnection()
-    
-    db = conn['Bank112']
-    collection = db['shares']
-
-    results = collection.delete_many({ 'total_owned': 0 })
-
-    print(results)
-
-    closeConnection(conn)
