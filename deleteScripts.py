00a47961239851d7bfe7f71dcc60eef23a9ca198
alecdayupay6 <alec.dayupay@student.ateneo.edu>
2024-12-03 17:00:15 +0800

    added delete script per collection

--- deleteScripts.py
+++ deleteScripts.py
@@ -1,6 +1,62 @@
 from utils import *
 
-# DELETE (FOR REVIEW)
+# DELETE
+
+# Deleting all customers
+def deleteCustomer():    
+    conn = openConnection()
+    
+    db = conn['Bank112']
+    collection = db['customer']
+
+    results = collection.delete_many({})
+    print(results)
+
+    closeConnection(conn)
+
+# Deleting all accounts
+def deleteAccount():    
+    conn = openConnection()
+    db = conn['Bank112']
+    collection = db['account']
+
+    results = collection.delete_many({})
+    print(results)
+
+    closeConnection(conn)
+
+# Deleting all issuers
+def deleteIssuer():
+    conn = openConnection()
+    db = conn['Bank112']
+    collection = db['issuer']
+
+    results = collection.delete_many({})
+    print(results)
+
+    closeConnection(conn)
+
+# Deleting all shares
+def deleteShares():    
+    conn = openConnection()
+    db = conn['Bank112']
+    collection = db['shares']
+
+    results = collection.delete_many({})
+    print(results)
+
+    closeConnection(conn)
+
+# Deleting all transactions
+def deleteTransaction():    
+    conn = openConnection()
+    db = conn['Bank112']
+    collection = db['transaction']
+
+    results = collection.delete_many({})
+    print(results)
+
+    closeConnection(conn)
 
 # Deleting shares documents without any shares owned
 def deleteEmptyShares():
